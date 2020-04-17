5614885c89d61d9f352bbf0e7b6421162465041d
Gan Jun Kai <kuhn96@gmail.com>
2020-04-17 22:26:28 +0800

    feat: add table and sidebar layout

--- index.tsx
+++ index.tsx
@@ -1,48 +1,154 @@
 import React from 'react';
 import {
-  FlipperPlugin,
   RoundedSection,
-  Button,
-  produce,
   CenteredView,
-  Info,
-  colors,
-  styled,
-  FlexRow,
+  FlipperPlugin,
+  Panel,
+  ManagedDataInspector,
   Text,
   brandColors,
+  TableBodyRow,
+  FlexColumn,
+  SearchableTable,
+  DetailSidebar
 } from 'flipper';
 
 type State = {
-  action: string;
+  selectedId: string; };
+
+type Row = {
+  type: string;
+  payload: object;
+}
+
+const columns = {
+  action: {
+    value: "Action"
+  }
+}
+
+const columnSizes = {
+  action: 'flex',
 };
 
-function initialState(): State {
-  return {
-    action: 'initialize',
+type PersistedState = {
+  actions: Array<any>,
+};
+
+export default class ReduxViewer extends FlipperPlugin<State, any, any> {
+  state = {
+    selectedId: ''
   };
-}
 
-export default class ReduxDevTools extends FlipperPlugin<State, any, any> {
-  state = initialState();
+  dummyData = [
+    {
+      id: "1",
+      action: "fetchStocksSuccess",
+      payload: {
+        item: "AAA"
+      }
+    },
+    {
+      id: "2",
+      action: "fetchOutletsSuccess",
+      payload: {
+        item: "BBB"
+      }
+    },
+  ]
 
-  componentDidMount() {
-    this.client.subscribe('newAction', (action) => {
-      this.setNewState(action);
-    });
+  static defaultPersistedState: PersistedState = {
+    actions: [],
+  };
+
+  static persistedStateReducer<PersistedState>(
+    persistedState: PersistedState,
+    method: string,
+    payload: Row,
+  ) {
+    console.log('payload: ', payload);
+    return {
+      ...persistedState,
+      actions: [
+        ...persistedState.actions,
+        payload
+      ]
+    }
+  };
+
+  renderSidebar() {
+    const { selectedId } = this.state;
+    if (selectedId != '') {
+      const { actions } = this.props.persistedState;
+      const selectedData = actions.find(v => v.type === selectedId);
+      console.log('selectedData: ', selectedData);
+      console.log('selectedId: ', selectedId);
+      return (
+        <Panel floating={false} heading={'Info'}>
+          <ManagedDataInspector data={selectedData} expandRoot={true} />
+        </Panel>
+      )
+    }
+
+    return null;
   }
 
-  setNewState(action) {
-    this.setState({ action: action.type });
+  buildRow(row: Row): TableBodyRow {
+    return {
+      columns: {
+        action: {
+          value: <Text>{row.type}</Text>,
+          filterValue: row.type,
+        },
+        payload: {
+          value: <Text>{JSON.stringify(row.payload)}</Text>,
+          filterValue: JSON.stringify(row.payload),
+        },
+      },
+      key: row.type,
+      copyText: JSON.stringify(row),
+      filterValue: `${row.type}`,
+    };
   }
+
+  onRowHighlighted = (key) => {
+    console.log("key: ", key);
+    this.setState({ selectedId: key[0] });
+  };
 
   render() {
+    const { actions } = this.props.persistedState;
+    console.log('actions: ', actions);
+    const rows = actions.map(v => this.buildRow(v));
+
     return (
-      <CenteredView>
-        <RoundedSection title="Redux Dev Tools">
-          <Text size={24}>Current action: {this.state.action}</Text>
-        </RoundedSection>
-      </CenteredView>
+      <FlexColumn grow={true}>
+        <SearchableTable
+          key={100}
+          rowLineHeight={28}
+          floating={false}
+          multiline={true}
+          columnSizes={columnSizes}
+          columns={columns}
+          onRowHighlighted={this.onRowHighlighted}
+          multiHighlight={false}
+          rows={rows}
+          stickyBottom={true}
+          /* actions={<Button onClick={this.clear}>Clear</Button>} */
+        />
+        <DetailSidebar>{this.renderSidebar()}</DetailSidebar>
+      </FlexColumn>
     );
   }
+
+  /* render() { */
+  /*   return ( */
+  /*     <CenteredView> */
+  /*       <RoundedSection title="Redux Dev Tools"> */
+  /*         <Text size={24}>Current action: {this.state.selectedId}</Text> */
+  /*       </RoundedSection> */
+  /*     </CenteredView> */
+  /*   ) */
+  /* } */
 }
+
